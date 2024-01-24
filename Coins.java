f5c9649a4b03221505c574314ad9ca02f9991499
dan-lilian <157275287+dan-lilian@users.noreply.github.com>
2024-01-24 23:41:48 +0200

    Add files via upload

--- Coins.java
+++ Coins.java
@@ -3,7 +3,7 @@ public class Coins {
 		int Totalcoins = Integer.parseInt(args[0]);
 		int Quarters = (Totalcoins/25);
     int Cents = (Totalcoins%25);
-		System.out.println(" use " + Quarters + " quarters and " + Cents + " cents");	
+		System.out.println(" Use " + Quarters + " quarters and " + Cents + " cents");	
     }
 
 }
