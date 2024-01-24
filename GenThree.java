dd963e25165333ede9e97b26389d7a15da93b43f
dan-lilian <157275287+dan-lilian@users.noreply.github.com>
2024-01-24 23:19:06 +0200

    Update GenThree.java

--- GenThree.java
+++ GenThree.java
@@ -1,6 +1,6 @@
 public class GenThree {
     public static void main(String[] args) {
-		int a = Integer.parseInt(args[0]);
+    int a = Integer.parseInt(args[0]);
     int b = Integer.parseInt(args[1]);
     int r1 = (int) (a + Math.random() * (b-a));
     int r2 = (int) (a + Math.random() * (b-a));
