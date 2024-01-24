3da758a9cab9e06c9cb75bf496ad0ba42b0eeef2
dan-lilian <157275287+dan-lilian@users.noreply.github.com>
2024-01-24 23:38:49 +0200

    Add files via upload

--- GenThreeTest.java
+++ GenThreeTest.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD:GenThreeTest.java
+
 public class GenThreeTest {
     public static void main(String[] args) {
 		int a = Integer.parseInt(args[0]);
@@ -11,21 +11,5 @@ public class GenThreeTest {
     System.out.println(r3);
     System.out.println("the minimal generated number was " + Math.min(r1, Math.min(r2, r3)));
     }
+  }
 
-}
-=======
-public class GenThree {
-    public static void main(String[] args) {
-    int a = Integer.parseInt(args[0]);
-    int b = Integer.parseInt(args[1]);
-    int r1 = (int) (a + Math.random() * (b-a));
-    int r2 = (int) (a + Math.random() * (b-a));
-    int r3 = (int) (a + Math.random() * (b-a));
-    System.out.println(r1);
-    System.out.println(r2);
-    System.out.println(r3);
-    System.out.println("the minimal generated number was " + Math.min(r1, Math.min(r2, r3)));
-    }
-
-}
->>>>>>> dd963e25165333ede9e97b26389d7a15da93b43f:GenThree.java
